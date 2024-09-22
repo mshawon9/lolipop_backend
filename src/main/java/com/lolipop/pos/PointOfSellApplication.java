e0f80631208b676c4a64bbf53f26bc49a3aba569
Mehedi Hasan Shawon <ms@vikingc.com>
2024-09-22 13:42:21 +0600

    First commit - added dependency, configure database and application.yml

---
+++ src/main/java/com/lolipop/pos/PointOfSellApplication.java
@@ -0,0 +1,13 @@
+package com.lolipop.pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointOfSellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointOfSellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lolipop/pos/PointOfSellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lolipop.pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointOfSellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
